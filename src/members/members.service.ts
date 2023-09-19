511c53d35205317163e1c0e52dac73391f1425fd
Joe Kariuki <jknguru6@gmail.com>
2023-09-19 14:58:29 +0300

    Add getMembers service logic

--- src/members/members.service.ts
+++ src/members/members.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MembersService {}
+export class MembersService {
+  private members = [
+    { id: 0, name: 'John Doe', sport: 'boxing' },
+    { id: 1, name: 'Jill Doe', sport: 'pilates' },
+  ];
+
+  getMembers(sport?: 'boxing' | 'pilates') {
+    if (sport) {
+      return this.members.filter((member) => member.sport === sport);
+    }
+    return this.members;
+  }
+}
