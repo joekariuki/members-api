292f29f7b2f3bfcada9f3b4b3965106bccc409fa
Joe Kariuki <jknguru6@gmail.com>
2023-09-19 14:34:48 +0300

    Add api entry points

--- src/members/members.controller.ts
+++ src/members/members.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
 @Controller('members')
-export class MembersController {}
+export class MembersController {
+  // GET /members
+  @Get()
+  getMembers(@Query('type') type: string) {
+    return [{ type }];
+  }
+  // GET /members/:id
+  @Get(':id')
+  getMember(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+  // POST /members
+  @Post()
+  createMember(@Body() createMemberDto) {
+    return {};
+  }
+  // PUT /members/:id
+  @Put(':id')
+  updateMember(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+  // DELETE /members/:id
+  @Delete(':id')
+  removeMember(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+}
